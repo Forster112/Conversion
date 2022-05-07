5f74c44a1006d090d1c2551161285c418b077bdf
Forster112 <forstermichael112@gmail.com>
2022-05-07 09:20:19 +0100

    added color beauty

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const binaSubmit = document.querySelector('#forbina');
 const answer = document.querySelector('.conde');
 const overlay = document.querySelector('.overlay');
 
+//colors selectors
+const body = document.querySelector('body');
+const p = document.querySelector('.p');
+const h1 = document.querySelector('h1');
+const color1 = document.querySelector('.color-1');
+const color2 = document.querySelector('.color-2');
+const color3 = document.querySelector('.color-3');
+const color4 = document.querySelector('.color-4');
+const color5 = document.querySelector('.color-5');
+
 // Re-usable function that reverses a string
 function reverseArr(input) {
     var ret = new Array;
@@ -77,4 +87,34 @@ binaSubmit.addEventListener('click', (e) => {
     e.preventDefault();
     rmHidden();
     decimal.value = bina(binary.value);
+})
+
+
+//body color changing
+color1.addEventListener('click', () => {
+    body.style.background = '#7f7f02';
+    body.style.backgroundAttachment = 'fixed';
+})
+
+color2.addEventListener('click', () => {
+    body.style.background = '#075761';
+    body.style.backgroundAttachment = 'fixed';
+    p.style.color = '#f4f4f4';
+})
+
+color3.addEventListener('click', () => {
+    body.style.background = '#185804';
+    body.style.backgroundAttachment = 'fixed';
+    p.style.color = '#f4f4f4';
+})
+
+color4.addEventListener('click', () => {
+    body.style.background = '#051d2b';
+    body.style.backgroundAttachment = 'fixed';
+    p.style.color = '#f4f4f4';
+})
+
+color5.addEventListener('click', () => {
+    body.style.background = '#000000';
+    body.style.backgroundAttachment = 'fixed';
 })
